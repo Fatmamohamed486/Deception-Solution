c4f5baf1fdc596c55c234a20a9098115b06cf88c
Fatmamohamed486 <101456811+Fatmamohamed486@users.noreply.github.com>
2024-01-19 21:21:09 +0200

    Update extract_ips.py

--- Floodlight Controller/CODE/extract_ips.py
+++ Floodlight Controller/CODE/extract_ips.py
@@ -4,18 +4,28 @@ def extract_snort_ip(alert_line):
     ip_pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)? -> (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?'
     match = re.search(ip_pattern, alert_line)
     if match:
-        print (f"Snort Source IP:{match.group(1)}")
+        #        print (f"Snort Source IP:{match.group(1)}")
         return match.group(1), match.group(2)
     else:
         return None, None
  
     
-def extract_apache_ip(log_line):
-    pattern = r'(\d+\.\d+\.\d+\.\d+) - - \[.*\] "GET /DVWA/(config|database)'
-    match = re.search(pattern, log_line)
+def extract_Modsec_ip(line):
+    # Define a regular expression pattern to match the client IP and port number
+    pattern = r"\[client (\d+\.\d+\.\d+\.\d+):(\d+)\]"
+
+    # Initialize variables to store client IP and port number
+    client_ip = None
+    port_number = None
+    # Use re.search to find the pattern in the log entry
+    match = re.search(pattern, line)
+
+    # Extract client IP and port number if a match is found
     if match:
-        #print(f"Apache Source IP: {match.group(1)}")
-        return match.group(1)
+        client_ip = match.group(1)
+        port_number = match.group(2)
     else:
-        print("No matches found.")
-        return None
+        print("No match found in log entry:", line)
+
+    # Return client IP and port number as variables
+    return client_ip, port_number
